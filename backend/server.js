4954159132a6c93e436ed5bced50e44a15c1f44a
EbraamWD <ebraam.saad@gmail.com>
2025-10-19 20:07:29 +0200

    slash for directory

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 // Hostaway API Configuration
