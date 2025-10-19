e4e5fc87f8efef50c606ea4fd623f885d7cdb4e8
EbraamWD <ebraam.saad@gmail.com>
2025-10-19 19:45:52 +0200

    redirect frontend on express

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +13,10 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 // Hostaway API Configuration
 const HOSTAWAY_CONFIG = {
   accountId: '61148',
